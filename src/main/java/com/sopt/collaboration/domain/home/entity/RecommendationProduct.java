cebb25889681acce312e5672b6c77e1bb6cdcae7
Jinsu Shin <jsshin8128@gmail.com>
2026-05-12 15:19:05 +0900

    feat(#9/home): 추천 API 엔티티 추가

---
+++ src/main/java/com/sopt/collaboration/domain/home/entity/RecommendationProduct.java
@@ -0,0 +1,36 @@
+package com.sopt.collaboration.domain.home.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "recommendation_products")
+public class RecommendationProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recommendation_section_id", nullable = false)
+    private RecommendationSection recommendationSection;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer displayOrder;
+}
